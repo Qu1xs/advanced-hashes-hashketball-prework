9a4fa1abe86046e2b7c1c303ce8ade090f2c3ca5
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-01 22:57:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,3 +9,25 @@
 
 
 
+def player_helper
+  game_hash[:home][:players].marge(game_hash[:away][:players])
+end
+
+def get_team_helper(player)
+  case team
+  when game_hash[:home][:team_name]
+    game_hash[:home]
+  when game_hash[:away][:team_name]
+    game_hash[:home]
+  end
+end
+
+def player_numbers(team)
+  get_team(team)[:players].map do |player|
+    player[:number]
+  end
+end
+
+def num_points_scored(player)
+  player_helper[:player][:score]
+end
